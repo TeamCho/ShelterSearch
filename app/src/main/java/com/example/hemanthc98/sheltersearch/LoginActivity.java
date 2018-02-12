de181244bd83784cadfcb84af6ea527cd97fdffa
hchittanuru3 <hemanthc98@outlook.com>
2018-02-11 23:05:26 -0500

    Initial Setup

--- app/src/main/java/com/example/hemanthc98/sheltersearch/LoginActivity.java
+++ app/src/main/java/com/example/hemanthc98/sheltersearch/LoginActivity.java
@@ -3,11 +3,13 @@ package com.example.hemanthc98.sheltersearch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
+
+
 }
---
+++ app/src/main/java/com/example/hemanthc98/sheltersearch/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.hemanthc98.sheltersearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    Button login;
+    Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        login = (Button) findViewById(R.id.login);
+        register = (Button) findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}

63aa6bff06f80f76513f4e2a5bfee0311d954949
luis-gzz <gamesbyluis@gmail.com>
2018-02-14 14:40:01 -0500

    Fixed a view UI issues

--- app/src/main/java/com/teamcho/sheltersearch/controllers/WelcomeActivity.java
+++ app/src/main/java/com/teamcho/sheltersearch/controllers/WelcomeActivity.java
@@ -19,6 +19,7 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        setTitle("");
         login = (Button) findViewById(R.id.login);
         register = (Button) findViewById(R.id.register);
 

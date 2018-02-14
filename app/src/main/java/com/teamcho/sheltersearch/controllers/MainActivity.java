eb757be0b31965dbd911cd66df308849b1f314a0
luis-gzz <gamesbyluis@gmail.com>
2018-02-14 00:07:23 -0500

    Login and Registration now works. Added Database class to hold user/pass

--- app/src/main/java/com/teamcho/sheltersearch/controllers/MainActivity.java
+++ app/src/main/java/com/teamcho/sheltersearch/controllers/MainActivity.java
@@ -1,10 +1,12 @@
-package com.teamcho.sheltersearch;
+package com.teamcho.sheltersearch.controllers;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.teamcho.sheltersearch.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         logout = (Button) findViewById(R.id.logout);
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent a = new Intent(view.getContext(), WelcomeActivity.class);
-                startActivity(a);
-            }
-        });
+    }
 
+    public void onClickLogout(View view) {
+        Intent a = new Intent(view.getContext(), WelcomeActivity.class);
+        startActivity(a);
     }
 }
---
+++ app/src/main/java/com/teamcho/sheltersearch/controllers/RegisterActivity.java
@@ -0,0 +1,61 @@
+package com.teamcho.sheltersearch.controllers;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.teamcho.sheltersearch.model.Database;
+import com.teamcho.sheltersearch.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    //Database backend;
+
+    EditText username;
+    EditText password;
+    Button register;
+    Button cancel;
+    TextView alert;
+
+    private String user;
+    private String pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        cancel = (Button) findViewById(R.id.cancel);
+        register = (Button) findViewById(R.id.register);
+        username = (EditText) findViewById(R.id.newUser);
+        password = (EditText) findViewById(R.id.newPass);
+        alert = (TextView) findViewById(R.id.alert);
+
+        //backend = new Database();
+
+        user = username.getText().toString();
+        pass = password.getText().toString();
+    }
+
+    public void onClickCancel(View view) {
+        Intent back = new Intent(view.getContext(), WelcomeActivity.class);
+        startActivity(back);
+    }
+
+    public void onClickRegister(View view) {
+        user = username.getText().toString();
+        pass = password.getText().toString();
+
+        boolean exists = Database.contains(user);
+        if (exists) {
+            alert.setText("Username is in use.");
+        } else if (user != "" && pass != "") {
+            Database.newEntry(user, pass);
+            Intent b = new Intent(view.getContext(), MainActivity.class);
+            startActivity(b);
+        }
+    }
+}

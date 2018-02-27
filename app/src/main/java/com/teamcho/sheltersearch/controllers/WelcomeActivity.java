15926603ef4d26e02d9528f1a36eb426770f92d5
luis-gzz <gamesbyluis@gmail.com>
2018-02-27 16:49:26 -0500

    Added shelter list view and shelter info page. Uses firebase database

--- app/src/main/java/com/teamcho/sheltersearch/controllers/WelcomeActivity.java
+++ app/src/main/java/com/teamcho/sheltersearch/controllers/WelcomeActivity.java
@@ -6,9 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.teamcho.sheltersearch.R;
 import com.teamcho.sheltersearch.controllers.LoginActivity;
 import com.teamcho.sheltersearch.controllers.RegisterActivity;
+import com.teamcho.sheltersearch.model.Database;
+import com.teamcho.sheltersearch.model.Shelter;
+
+import java.util.ArrayList;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -20,9 +29,13 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         setTitle("");
+
+        if (Database.getShelterList() == null && Database.getShelterNameList() == null) {
+            Database.loadData();
+        }
+
         login = (Button) findViewById(R.id.login);
         register = (Button) findViewById(R.id.register);
-
     }
 
     public void onClickLogin(View view) {

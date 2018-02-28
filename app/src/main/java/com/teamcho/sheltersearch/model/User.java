9298d7bd0103bd7b0259f90ac91e7dec067f0fb8
rzhade3 <rzhade@gatech.edu>
2018-02-27 21:15:37 -0500

    Created two new classes for User and for UserType enum

---
+++ app/src/main/java/com/teamcho/sheltersearch/model/User.java
@@ -0,0 +1,43 @@
+package com.teamcho.sheltersearch.model;
+
+/**
+ * Created by rahulzhade on 2/27/18.
+ */
+
+public class User {
+    private String uid;
+    private String name;
+    private String email;
+    private UserType userType;
+
+    public User(String uid, String name, String email, UserType userType) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.userType = userType;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    public String getUserType() {
+        return this.userType.getUserType();
+    }
+}
